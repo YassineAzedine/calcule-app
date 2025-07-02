42006526a3adaeecd8738993ceed42003b89899e
YassineAzedine <yassine.azedine0711@gmail.com>
2025-07-02 10:37:34 +0100

    last test

--- calcule.test.js
+++ calcule.test.js
@@ -1,7 +1,7 @@
 const { add, sub } = require('./calcule');
 
 test('addition de 2 et 3', () => {
-  expect(add(2, 3)).toBe(6);
+  expect(add(2, 3)).toBe(5);
 });
 
 test('soustraction de 5 et 2', () => {
